08929930ae0a24f6c693e0d843c8739a7c66b140
Nosferatu <davonb113@gmail.com>
2022-03-04 15:11:27 -0600

    added README and debugged

---
+++ README.md
@@ -0,0 +1,15 @@
+# Coding Quiz App
+
+App designed to showcase JavaScripts ability to display information, record information, render information, and store information.
+
+## Installation
+
+Application is usable on every browser.
+
+## Screenshots 
+
+[!Screenshot](./assets/images/quiz.png)
+
+[!Screenshot](./assets/images/quiza.png)
+
+[!Screenshot](./assets/images/hs.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-//Welcome Page Elements
+// Welcome Page Elements 
 const welcomeEl = document.querySelector("#welcome");
-const StartQuizBtnEl = document.querySelector("#startQuiz");
+const startQuizBtnEl = document.querySelector("#startQuiz");
 
-//Quiz Pages Elements
+//Quiz Page Elements 
 const quizEl = document.querySelector("#quiz");
 const questionEl = document.querySelector("#question");
 const answersEl = document.querySelector("#answers");
 
-//Input Score Page Elements 
+//Input Score Page Elements
 const inputScoreEl = document.querySelector("#inputScore");
 const initialsEl = document.querySelector("#initials");
-const submitInitialsEl = document.querySelector("#submitInitials");
+const submitInitialsBtnEl = document.querySelector("#submitInitials");
 const userScoreEl = document.querySelector("#score");
 
 //View High Scores Page Elements
@@ -19,20 +19,20 @@ const scoresEl = document.querySelector("#scores");
 const goBackBtnEl = document.querySelector("#goBack");
 const clearScoresBtnEl = document.querySelector("#clearScores");
 
-//Universal var and const
+//Universal vars 
 const viewHScoresBtnEl = document.querySelector("#viewHScores");
 const timerEl = document.querySelector("#timer");
 var score = 0;
 var currentQ = 0;
 var highScores = [];
 var interval;
-var timeGiven = 60;
+var timeGiven = 75;
 var secondsElapsed = 0;
 
-//start and updates timer
+//starts and updates timer
 function startTimer() {
     timerEl.textContent = timeGiven;
-    interval = setInterval(function() {
+    interval = setInterval(function () {
         secondsElapsed++;
         timerEl.textContent = timeGiven - secondsElapsed;
         if (secondsElapsed >= timeGiven) {
@@ -42,20 +42,20 @@ function startTimer() {
     }, 1000);
 }
 
-//stops timer 
+//stops timer
 function stopTimer() {
     clearInterval(interval);
 }
 
-//Clears current question and calls for display fo next question
-//Calls for input score display of last question
+//Clears current question and calls for display of next question
+//Calls for input score display if last question
 function nextQuestion() {
     currentQ++;
     if (currentQ < questions.length) {
         renderQuestion();
     } else {
         stopTimer();
-        if ((timeGiven - secondsElapsed) > 0) 
+        if ((timeGiven - secondsElapsed) > 0)
             score += (timeGiven - secondsElapsed);
         userScoreEl.textContent = score;
         hide(quizEl);
@@ -64,28 +64,30 @@ function nextQuestion() {
     }
 }
 
-//Checks answer and updates score
+//checks answer based on current question and updates the user score
 function checkAnswer(answer) {
     if (questions[currentQ].answer == questions[currentQ].choices[answer.id]) {
         score += 5;
         displayMessage("Correct!");
-    } else {
+    }
+    else {
         secondsElapsed += 10;
-        displayMessage("Wrong!");
+        displayMessage("Wrong...");
     }
 }
 
-//displays message for 2 seconds
+//displays a message for 2 seconds
 function displayMessage(m) {
     let messageHr = document.createElement("hr");
     let messageEl = document.createElement("div");
     messageEl.textContent = m;
     document.querySelector(".jumbotron").appendChild(messageHr);
     document.querySelector(".jumbotron").appendChild(messageEl);
-    setTimeout(function() {
-        messageHr.remove();
-        messageEl.remove();
+    setTimeout(function () {
+            messageHr.remove();
+            messageEl.remove();
     }, 2000);
+
 }
 
 //hides element
@@ -98,7 +100,7 @@ function show(element) {
     element.style.display = "block";
 }
 
-//reset variables 
+//reset local variables
 function reset() {
     score = 0;
     currentQ = 0;
@@ -106,7 +108,7 @@ function reset() {
     timerEl.textContent = 0;
 }
 
-//Renders current question 
+//Renders current question
 function renderQuestion() {
     questionEl.textContent = questions[currentQ].title;
     for (i = 0; i < answersEl.children.length; i++) {
@@ -114,23 +116,23 @@ function renderQuestion() {
     }
 }
 
-//Renders high scores in local storage 
+//Renders high scores stored in local storage
 function renderHighScores() {
-    //clear content
-    highScoresEl.innerHTML = "";
+    // Clear content
+    scoresEl.innerHTML = "";
     show(highScoresEl);
     highScores = JSON.parse(localStorage.getItem("scores"));
     for (let i = 0; i < highScores.length; i++) {
         let scoreItem = document.createElement("div");
-        scoreItem.className += "row";
+        scoreItem.className += "row mb-3 p-2";
         console.log(scoreItem)
-        scoreItem.setAttribute("style", "background-color:Yellow;");
+        scoreItem.setAttribute("style", "background-color:PaleTurquoise;");
         scoreItem.textContent = `${(i + 1)}. ${highScores[i].username} - ${highScores[i].userScore}`;
         scoresEl.appendChild(scoreItem);
     }
 }
 
-//display high scores
+//displays high scores
 viewHScoresBtnEl.addEventListener("click", function () {
     hide(welcomeEl);
     hide(quizEl);
@@ -140,26 +142,26 @@ viewHScoresBtnEl.addEventListener("click", function () {
     reset();
 });
 
-//starts quiz from welcome page 
-StartQuizBtnEl.addEventListener("click", function() {
+//starts quiz from  Welcome page
+startQuizBtnEl.addEventListener("click", function () {
     hide(welcomeEl);
     startTimer();
     renderQuestion();
     show(quizEl);
 });
 
-//calls to check answer selected and calls to next question if button is clicked
-answersEl.addEventListener("click", function(e) {
+//Calls to check answer selected and calls to next question if button is clicked
+answersEl.addEventListener("click", function (e) {
     if (e.target.matches("button")) {
         checkAnswer(e.target);
         nextQuestion();
     }
 });
 
-//creates user score obj to push to local storage scores array
-//calls to render high scores
+//Creates a user score object to push to the local storage scores array 
 //calls to display high scores
-submitInitialsEl.addEventListener("click", function() {
+//calls to render high scores
+submitInitialsBtnEl.addEventListener("click", function () {
     let initValue = initialsEl.value.trim();
     if (initValue) {
         let userScore = { username: initValue, userScore: score };
@@ -173,14 +175,14 @@ submitInitialsEl.addEventListener("click", function() {
     }
 });
 
-//goes back to welcome page from high scores
-goBackBtnEl.addEventListener("click", function() {
+//Goes back to Welcome page from High scores 
+goBackBtnEl.addEventListener("click", function () {
     hide(highScoresEl);
     show(welcomeEl);
 });
 
-//clears saved scores from local storage 
-clearScoresBtnEl.addEventListener("click", function(){
+//Clears saved scores from local storage
+clearScoresBtnEl.addEventListener("click", function () {
     highScores = [];
     localStorage.setItem("scores", JSON.stringify(highScores));
     renderHighScores();
